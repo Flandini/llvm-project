7c67c8fc5a909050a6308815977dafff56bc138e
Balazs Benics <benicsbalazs@gmail.com>
2025-02-02 19:34:53 +0100

    Simplify RetainCountDiagnostics

--- clang/lib/StaticAnalyzer/Checkers/RetainCountChecker/RetainCountDiagnostics.cpp
+++ clang/lib/StaticAnalyzer/Checkers/RetainCountChecker/RetainCountDiagnostics.cpp
@@ -690,11 +690,9 @@ static AllocationInfo GetAllocationSite(ProgramStateManager &StateMgr,
       const MemRegion *R = FB.getRegion();
       // Do not show local variables belonging to a function other than
       // where the error is reported.
-      const StackSpaceRegion *SSR =
-          dyn_cast_if_present<StackSpaceRegion>(R->getMemorySpace(St));
-
-      if (SSR && SSR->getStackFrame() == LeakContext->getStackFrame())
-        FirstBinding = R;
+      if (const auto *MR = dyn_cast<StackSpaceRegion>(R->getMemorySpace(St)))
+        if (MR->getStackFrame() == LeakContext->getStackFrame())
+          FirstBinding = R;
     }
 
     // AllocationNode is the last node in which the symbol was tracked.
