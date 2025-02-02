a58845163a6ba4f259aff927a6e0f4fbea335480
Balazs Benics <benicsbalazs@gmail.com>
2025-02-02 19:31:28 +0100

    Uplift PutenvStackArrayChecker

--- clang/lib/StaticAnalyzer/Checkers/PutenvStackArrayChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/PutenvStackArrayChecker.cpp
@@ -45,13 +45,10 @@ void PutenvStackArrayChecker::checkPostCall(const CallEvent &Call,
   SVal ArgV = Call.getArgSVal(0);
   const Expr *ArgExpr = Call.getArgExpr(0);
 
-  const MemRegion *MR = ArgV.getAsRegion();
-
-  const StackSpaceRegion *SSR =
-      dyn_cast_if_present<StackSpaceRegion>(MR->getMemorySpace(C.getState()));
+  const auto *SSR = dyn_cast<StackSpaceRegion>(
+      ArgV.getAsRegion()->getMemorySpace(C.getState()));
   if (!SSR)
     return;
-
   const auto *StackFrameFuncD =
       dyn_cast_or_null<FunctionDecl>(SSR->getStackFrame()->getDecl());
   if (StackFrameFuncD && StackFrameFuncD->isMain())
